5e8faa7393f1ab6ce38ea680074ef37801fa4954
jacksoncook <jacksoncook73@gmail.com>
2018-12-28 15:42:43 -0500

    Restylized login screen and added register button

--- screens/LoginComponent.js
+++ screens/LoginComponent.js
@@ -29,77 +29,91 @@ export default class LoginComponent extends React.Component {
         <View style={{
             padding: 10,
             backgroundColor: 'white',
+            flex: 1,
         }}>
-            <Image
-              source={
-                require('../assets/images/gtRideshareLogo.png')
-              }
-            />
-            <View style={styles.loginInputs}>
-                <TextInput
-                    style={{height: 40, backgroundColor: 'white'}}
-                    placeholder="Email (swag@gatech.edu)"
-                    onChangeText={(userEmail) => this.setState({userEmail})}
-                />
-            </View>
-            <View style={styles.loginInputs}>
-                <TextInput
-                    style={{height: 40, backgroundColor: 'white',}}
-                    secureTextEntry={true}
-                    placeholder="Password"
-                    onChangeText={(password) => this.setState({password})}
-                />
-            </View>
-            <Button
+          <Image
+            source={
+              require('../assets/images/gtRideshareLogo.png')
+            }
+            style ={{
+              flex: 5,
+              resizeMode: 'contain'
+            }}
+          />
+          <View style={styles.loginInputs}>
+              <TextInput
+                style={styles.textInputs}
+                placeholder="Email (...@gatech.edu)"
+                onChangeText={(userEmail) => this.setState({userEmail})}
+              />
+          </View>
+          <View style={styles.loginInputs}>
+              <TextInput
+                style={styles.textInputs}
+                secureTextEntry={true}
+                placeholder="Password"
+                onChangeText={(password) => this.setState({password})}
+              />
+          </View>
+          <View style = {{
+            flex: 1,
+            flexDirection: 'row',
+            paddingVertical: 20,
+            justifyContent: 'center',
+            alignItems: 'center'}}>
+              <Button
                 onPress={() => {
                     Alert.alert('You tapped the button!');
                 }}
-                style={{paddingRight: 80, paddingLeft: 80}}
+                style={{flex: 1,}}
+                color='#F5D580'
                 title="Log In"
             />
+          </View>
+          <View style = {{
+            flex: 1,
+            flexDirection: 'row',
+            paddingVertical: 20,
+            justifyContent: 'flex-end',
+            alignItems: 'center'}}>
+              <Text>New user?</Text>
+              <Button
+                  onPress={() => {
+                      this.props.register();
+                  }}
+                  color='#004F9F'
+                  title="Register"
+              />
+          </View>
         </View>
       );
     }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
 }
 const styles = StyleSheet.create({
     loginInputs: {
-        paddingRight: 40,
-        paddingLeft: 40,
-        height: 40
+      paddingRight: 20,
+      paddingLeft: 20,
+      flex: 1,
+      paddingVertical: 5,
+    },
+
+    textInputs: {
+      backgroundColor: 'white',
+      borderColor: '#C4C4C4',
+      borderWidth: .5,
+      borderRadius: 4,
     },
 
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
+      flex: 1,
+      backgroundColor: '#fff',
     },
     developmentModeText: {
-        marginBottom: 20,
-        color: 'rgba(0,0,0,0.4)',
-        fontSize: 14,
-        lineHeight: 19,
-        textAlign: 'center',
+      marginBottom: 20,
+      color: 'rgba(0,0,0,0.4)',
+      fontSize: 14,
+      lineHeight: 19,
+      textAlign: 'center',
 },
 });
