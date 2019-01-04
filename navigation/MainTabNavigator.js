963c2b546eea2110250a2936b86782e8a85b1654
jacksoncook <jacksoncook73@gmail.com>
2019-01-04 15:54:07 -0500

    Removed login from tab navigator.

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -6,31 +6,12 @@ import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
-import NoNavBarContainer from '../screens/NoNavBarContainer';
 
 
 {/* 
   This component creates the logged in view.  Currently it is being used
   to test additions such as the login screen in this case
 */} 
-
-const LoginStack = createStackNavigator({
-  Login: NoNavBarContainer,
-});
-
-LoginStack.navigationOptions = {
-  tabBarLabel: 'Login',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -80,7 +61,6 @@ SettingsStack.navigationOptions = {
 
 export default createBottomTabNavigator({
   HomeStack,
-  LoginStack,
   LinksStack,
   SettingsStack,
 });
