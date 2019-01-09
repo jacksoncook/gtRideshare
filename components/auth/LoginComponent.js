d334d062dc59193026fbddbfc6c07dd2199b1e04
jacksoncook <jacksoncook73@gmail.com>
2019-01-09 10:07:14 -0500

    Fixed bug in login caused by new linter

--- components/auth/LoginComponent.js
+++ components/auth/LoginComponent.js
@@ -21,77 +21,74 @@ export default class LoginComponent extends React.Component {
   }
 
   onLoginPress = () => {
-    firebase.auth().signInWithEmailAndPassword(this.state.userEmail, this.state.password)
+    firebase.auth().signInWithEmailAndPassword(this.state.userEmail.trim(), this.state.password)
     .then(() => { }, (error) => {
       Alert.alert(error.message);
     });
   }
 
   render() {
-      return (
-        <View style={{
-            padding: 10,
-            backgroundColor: 'white',
-            flex: 1,
-        }}>
-          <Image
-            source={
-              require('../../assets/images/gtRideshareLogo.png')
-            }
-            style ={{
-              flex: 5,
-              resizeMode: 'contain'
-            }}
-          />
-          <View style={styles.loginInputs}>
-              <TextInput
-                style={styles.textInputs}
-                placeholder="Email (...@gatech.edu)"
-                onChangeText={(text) => this.setState({userEmail: text})}
-              />
-          </View>
-          <View style={styles.loginInputs}>
-              <TextInput
-                style={styles.textInputs}
-                secureTextEntry={true}
-                placeholder="Password"
-                onChangeText={(text) => this.setState({password: text})}
-              />
-          </View>
-          <View style = {{
-            flex: 1,
-            flexDirection: 'row',
-            paddingVertical: 20,
-            justifyContent: 'center',
-            alignItems: 'center'}}>
-              <Button
-                onPress={() => {
-                    this.onLoginPress;
-                }}
-                style={{flex: 1,}}
-                color='#F5D580'
-                title="Log In"
+    return (
+      <View style={{
+        padding: 10,
+        backgroundColor: 'white',
+        flex: 1,
+      }}>
+        <Image
+          source={
+            require('../../assets/images/gtRideshareLogo.png')
+          }
+          style ={{
+            flex: 5,
+            resizeMode: 'contain'
+          }}
+        />
+        <View style={styles.loginInputs}>
+            <TextInput
+              style={styles.textInputs}
+              placeholder="Email (...@gatech.edu)"
+              onChangeText={(text) => this.setState({userEmail: text})}
             />
-          </View>
-          <View style = {{
-            flex: 1,
-            flexDirection: 'row',
-            paddingVertical: 20,
-            justifyContent: 'flex-end',
-            alignItems: 'center'}}>
-              <Text>New user?</Text>
-              <Button
-                  onPress={() => {
-                      this.props.register();
-                  }}
-                  color='#004F9F'
-                  title="Register"
-              />
-          </View>
         </View>
-      );
-    }
-
+        <View style={styles.loginInputs}>
+            <TextInput
+              style={styles.textInputs}
+              secureTextEntry={true}
+              placeholder="Password"
+              onChangeText={(text) => this.setState({password: text})}
+            />
+        </View>
+        <View style = {{
+          flex: 1,
+          flexDirection: 'row',
+          paddingVertical: 20,
+          justifyContent: 'center',
+          alignItems: 'center'}}>
+            <Button
+              onPress={this.onLoginPress}
+              style={{flex: 1,}}
+              color='#F5D580'
+              title="Log In"
+          />
+        </View>
+        <View style = {{
+          flex: 1,
+          flexDirection: 'row',
+          paddingVertical: 20,
+          justifyContent: 'flex-end',
+          alignItems: 'center'}}>
+            <Text>New user?</Text>
+            <Button
+                onPress={() => {
+                    this.props.register();
+                }}
+                color='#004F9F'
+                title="Register"
+            />
+        </View>
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
   loginInputs: {
