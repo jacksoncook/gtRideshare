fd90ee93fba1240221ff4acc14577d81c27ea91d
jacksoncook <jacksoncook73@gmail.com>
2019-01-28 10:39:37 -0500

    Restyled code for improved readability

--- components/auth/LoginComponent.js
+++ components/auth/LoginComponent.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 import {
   Alert,
@@ -9,87 +12,7 @@ import {
   View,
 } from 'react-native';
 import * as firebase from 'firebase';
-{/* 
-  This component contains the login form
-*/} 
 
-export default class LoginComponent extends React.Component {
-  
-  constructor(props) {
-      super(props);
-      this.state = {userEmail: '', password: ''};
-  }
-
-  onLoginPress = () => {
-    firebase.auth().signInWithEmailAndPassword(this.state.userEmail.trim(), this.state.password)
-    .then(() => { }, (error) => {
-      Alert.alert(error.message);
-    });
-  }
-
-  render() {
-    return (
-      <View style={{
-        padding: 10,
-        backgroundColor: 'white',
-        flex: 1,
-      }}>
-        <Image
-          source={
-            require('../../assets/images/gtRideshareLogo.png')
-          }
-          style ={{
-            flex: 5,
-            resizeMode: 'contain'
-          }}
-        />
-        <View style={styles.loginInputs}>
-            <TextInput
-              style={styles.textInputs}
-              placeholder="Email (...@gatech.edu)"
-              onChangeText={(text) => this.setState({userEmail: text})}
-            />
-        </View>
-        <View style={styles.loginInputs}>
-            <TextInput
-              style={styles.textInputs}
-              secureTextEntry={true}
-              placeholder="Password"
-              onChangeText={(text) => this.setState({password: text})}
-            />
-        </View>
-        <View style = {{
-          flex: 1,
-          flexDirection: 'row',
-          paddingVertical: 20,
-          justifyContent: 'center',
-          alignItems: 'center'}}>
-            <Button
-              onPress={this.onLoginPress}
-              style={{flex: 1,}}
-              color='#F5D580'
-              title="Log In"
-          />
-        </View>
-        <View style = {{
-          flex: 1,
-          flexDirection: 'row',
-          paddingVertical: 20,
-          justifyContent: 'flex-end',
-          alignItems: 'center'}}>
-            <Text>New user?</Text>
-            <Button
-                onPress={() => {
-                    this.props.register();
-                }}
-                color='#004F9F'
-                title="Register"
-            />
-        </View>
-      </View>
-    );
-  }
-}
 const styles = StyleSheet.create({
   loginInputs: {
     paddingRight: 20,
@@ -101,7 +24,89 @@ const styles = StyleSheet.create({
   textInputs: {
     backgroundColor: 'white',
     borderColor: '#C4C4C4',
-    borderWidth: .5,
+    borderWidth: 0.5,
     borderRadius: 4,
   },
 });
+
+// This component contains the login form
+
+export default class LoginComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userEmail: '', password: '' };
+    this.onLoginPress = this.onLoginPress.bind(this);
+  }
+
+  onLoginPress() {
+    const { userEmail, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(userEmail.trim(), password)
+      .then(() => { }, (error) => {
+        Alert.alert(error.message);
+      });
+  }
+
+  render() {
+    return (
+      <View style={{
+        padding: 10,
+        backgroundColor: 'white',
+        flex: 1,
+      }}
+      >
+        <Image
+          source={require('../../assets/images/gtRideshareLogo.png')}
+          style={{
+            flex: 5,
+            resizeMode: 'contain',
+          }}
+        />
+        <View style={styles.loginInputs}>
+          <TextInput
+            style={styles.textInputs}
+            placeholder="Email (...@gatech.edu)"
+            onChangeText={text => this.setState({ userEmail: text })}
+          />
+        </View>
+        <View style={styles.loginInputs}>
+          <TextInput
+            style={styles.textInputs}
+            secureTextEntry
+            placeholder="Password"
+            onChangeText={text => this.setState({ password: text })}
+          />
+        </View>
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          paddingVertical: 20,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        >
+          <Button
+            onPress={this.onLoginPress}
+            style={{ flex: 1 }}
+            color="#F5D580"
+            title="Log In"
+          />
+        </View>
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          paddingVertical: 20,
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        }}
+        >
+          <Text>New user?</Text>
+          <Button
+            onPress={this.props.register}
+            color="#004F9F"
+            title="Register"
+          />
+        </View>
+      </View>
+    );
+  }
+}
--- components/auth/RegistrationComponent.js
+++ components/auth/RegistrationComponent.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 import {
   Alert,
@@ -8,141 +11,156 @@ import {
   View,
 } from 'react-native';
 import * as firebase from 'firebase';
+
+const styles = StyleSheet.create({
+  registrationInputs: {
+    paddingRight: 20,
+    paddingLeft: 20,
+    flex: 1,
+  },
+  textInputs: {
+    backgroundColor: 'white',
+    borderColor: '#C4C4C4',
+    borderWidth: 0.5,
+    borderRadius: 4,
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+});
+
 const FIREBASE_ATTRIBUTES = require('../../constants/FirebaseAttributes');
-{/* 
-  This component contains the Registration form
-*/}
+
+// This component contains the Registration form
 
 export default class RegistrationComponent extends React.Component {
-    
   constructor(props) {
-      super(props);
-      this.state = {
-        firstName: '',
-        lastName: '',
-        userEmail: '',
-        password: '',
-        confirmedPassword: '',
+    super(props);
+    this.state = {
+      userFirstName: '',
+      userLastName: '',
+      userEmail: '',
+      password: '',
+      confirmedPassword: '',
     };
+    this.onRegisterPress = this.onRegisterPress.bind(this);
   }
-  
-  onRegisterPress = () => {
-      if (this.state.password === this.state.confirmedPassword) {
-        firebase.auth().createUserWithEmailAndPassword(this.state.userEmail.trim(), this.state.password)
-          .then(() => { 
-              firebase.database().ref(FIREBASE_ATTRIBUTES.USERS + '/' + firebase.auth().currentUser.uid).set({
-                  firstName: this.state.firstName,
-                  lastName: this.state.lastName,
-                  email: this.state.userEmail,
-                  phoneNumber: 'XXX-XXX-XXXX',
-                  rides: 0,
-                  posts: 0,
-              });
-          }, (error) => {
-              Alert.alert(error.message);
+
+  onRegisterPress() {
+    const {
+      userFirstName,
+      userLastName,
+      userEmail,
+      password,
+      confirmedPassword,
+    } = this.state;
+    if (password === confirmedPassword) {
+      firebase.auth()
+        .createUserWithEmailAndPassword(userEmail.trim(), password)
+        .then(() => {
+          const uid = firebase.auth().currentUser.uid;
+          firebase.database().ref(`${FIREBASE_ATTRIBUTES.USERS}/${uid}`).set({
+            firstName: userFirstName,
+            lastName: userLastName,
+            email: userEmail,
+            phoneNumber: 'XXX-XXX-XXXX',
+            rides: 0,
+            postCount: 0,
+            uID: uid,
           });
-      } else {
-          Alert.alert("Passwords do not match");
-          return;
-      }
+        }, (error) => {
+          Alert.alert(error.message);
+        });
+    } else {
+      Alert.alert('Passwords do not match');
+    }
   }
 
   render() {
-      return (
-        <View style={{
-            padding: 10,
-            backgroundColor: 'white',
-            flex: 1,
-        }}>
-            <View style={styles.registrationInputs}>
-                <TextInput
-                    style={styles.textInputs}
-                    placeholder="First Name"
-                    onChangeText={(text) => this.setState({firstName: text})}
-                />
-            </View>
-            <View style={styles.registrationInputs}>
-                <TextInput
-                    style={styles.textInputs}
-                    placeholder="LastName"
-                    onChangeText={(text) => this.setState({lastName: text})}
-                />
-            </View>
-            <View style={styles.registrationInputs}>
-                <TextInput
-                    style={styles.textInputs}
-                    placeholder="GT Email (...@gatech.edu)"
-                    onChangeText={(text) => this.setState({userEmail: text})}
-                />
-            </View>
-            <View style={styles.registrationInputs}>
-                <TextInput
-                    style={styles.textInputs}
-                    secureTextEntry={true}
-                    placeholder="Password"
-                    onChangeText={(text) => this.setState({password: text})}
-                />
-            </View>
-            <View style={styles.registrationInputs}>
-                <TextInput
-                    style={styles.textInputs}
-                    secureTextEntry={true}
-                    placeholder="Confirm Password"
-                    onChangeText={(text) => this.setState({confirmedPassword: text})}
-                />
-            </View>
-            <View style = {{
-              flex: 1,
-              flexDirection: 'row',
-              paddingVertical: 20,
-              justifyContent: 'center',
-              alignItems: 'center'}}>
-              <Button
-                onPress={this.onRegisterPress}
-                color='#F5D580'
-                style={{paddingRight: 80, paddingLeft: 80, flex: 1}}
-                title="Register"
-            />
-            </View>
-            <View style = {{
-              flex: 1,
-              flexDirection: 'row',
-              paddingVertical: 20,
-              justifyContent: 'flex-end',
-              alignItems: 'center'}}>
-                <Text>Already have an account?</Text>
-                <Button
-                  onPress={this.props.returnToLogin}
-                  color='#004F9F'
-                  title="Return to Login"
-                />
-            </View>
+    return (
+      <View style={{
+        padding: 10,
+        backgroundColor: 'white',
+        flex: 1,
+      }}
+      >
+        <View style={styles.registrationInputs}>
+          <TextInput
+            style={styles.textInputs}
+            placeholder="First Name"
+            onChangeText={text => this.setState({ userFirstName: text })}
+          />
         </View>
-      );
-    }
+        <View style={styles.registrationInputs}>
+          <TextInput
+            style={styles.textInputs}
+            placeholder="LastName"
+            onChangeText={text => this.setState({ userLastName: text })}
+          />
+        </View>
+        <View style={styles.registrationInputs}>
+          <TextInput
+            style={styles.textInputs}
+            placeholder="GT Email (...@gatech.edu)"
+            onChangeText={text => this.setState({ userEmail: text })}
+          />
+        </View>
+        <View style={styles.registrationInputs}>
+          <TextInput
+            style={styles.textInputs}
+            secureTextEntry
+            placeholder="Password"
+            onChangeText={text => this.setState({ password: text })}
+          />
+        </View>
+        <View style={styles.registrationInputs}>
+          <TextInput
+            style={styles.textInputs}
+            secureTextEntry
+            placeholder="Confirm Password"
+            onChangeText={text => this.setState({ confirmedPassword: text })}
+          />
+        </View>
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          paddingVertical: 20,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        >
+          <Button
+            onPress={this.onRegisterPress}
+            color="#F5D580"
+            style={{ paddingRight: 80, paddingLeft: 80, flex: 1 }}
+            title="Register"
+          />
+        </View>
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          paddingVertical: 20,
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        }}
+        >
+          <Text>Already have an account?</Text>
+          <Button
+            onPress={this.props.returnToLogin}
+            color="#004F9F"
+            title="Return to Login"
+          />
+        </View>
+      </View>
+    );
+  }
 }
-const styles = StyleSheet.create({
-    registrationInputs: {
-        paddingRight: 20,
-        paddingLeft: 20,
-        flex: 1,
-    },
-    textInputs: {
-      backgroundColor: 'white',
-      borderColor: '#C4C4C4',
-      borderWidth: .5,
-      borderRadius: 4,
-    },
-
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    developmentModeText: {
-        marginBottom: 20,
-        color: 'rgba(0,0,0,0.4)',
-        fontSize: 14,
-        lineHeight: 19,
-        textAlign: 'center',
-},
-});
